6b91ab4dcfcacb3415987fc0eb59504c7bc08b0f
hubalichanna <channahubali95@gmail.com>
2025-02-17 16:57:11 +0500

    ..

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+
+import NavBar from './components/NavBar'
+
+
+function App() {
+  
+  return (
+    
+    <div>
+      <NavBar/>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favourites' element={<Favourites/>}/>
+      </Routes> 
+
+    </main>
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,15 @@
+import '../css/Favorites.css';
+
+function Favourites(){
+    return(
+        <>
+        <div className="favourites-empty">
+            <h2>No favourite Movies Yet</h2>
+            <p>Start adding movies to your favourites and they will apear here</p>
+
+        </div>
+        </>
+    );
+}
+
+export default Favourites;
